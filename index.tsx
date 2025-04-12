ee158f60eab84c21b3b1aeb74325f3846a194bca
GoshoMosho <151710136+GoshoMosho@users.noreply.github.com>
2025-04-11 18:54:44 -0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <h1>🔥 BannerMatch MVP Live!</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
